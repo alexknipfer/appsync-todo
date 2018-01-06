e553bcf97ea465994714ca773be3588f5aa2f02f
alexknipfer <alexanderknipfer@gmail.com>
2018-01-06 16:28:28 -0600

    Added aws config

---
+++ src/lib/awsConfig.js
@@ -0,0 +1,11 @@
+const config = {
+    AWS_ACCESS_KEY_ID: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+    HOST: process.env.REACT_APP_AWS_HOST,
+    REGION: process.env.REACT_APP_AWS_REGION,
+    PATH: '/graphql'
+}
+
+config.ENDPOINT = `https://${config.HOST}${config.PATH}`
+
+export default config
