2c762b5ffef8765dc8d1cf10c99866df55c33c3c
alexknipfer <alexanderknipfer@gmail.com>
2018-01-08 22:14:18 -0600

    Basic layout

--- src/routes/Todos/index.js
+++ src/routes/Todos/index.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import { graphql, compose } from 'react-apollo'
 import uuidv4 from 'uuid/v4'
+import { DefaultButton, TextField } from 'office-ui-fabric-react/lib'
+import {
+    FocusZone,
+    FocusZoneDirection
+  } from 'office-ui-fabric-react/lib/FocusZone'
+import { List } from 'office-ui-fabric-react/lib/List';
+import styled from 'styled-components'
 
 import AllTodosQuery from './queries/allTodosQuery'
 import CreateTodoMutation from './mutations/createTodoMutation'
 import NewTodoSubscription from './subscriptions/newTodoSubscription'
+
+const AddTodoInputSection = styled.div`
+    display: flex;
+`
 
 class Todos extends Component {
     componentWillMount() {
@@ -30,14 +41,31 @@ class Todos extends Component {
             <div>
                 <h3>Todos</h3>
 
-                <input type='text' placeholder='Todo Name' id='todoName' />
-                <input type='text' placeholder='Todo Description' id='todoDescription' />
+                <AddTodoInputSection>
+                    <TextField type='text' placeholder='Todo Name' id='todoName' />
+                    <TextField type='text' placeholder='Todo Description' id='todoDescription' />
+                    <DefaultButton
+                        primary={ true }
+                        data-automation-id='test'
+                        text='Create Todo'
+                        onClick={() => this.createTodo()}
+                    />
+                </AddTodoInputSection>
 
-                <button onClick={() => this.createTodo()}>Create Todo</button>
+                {allTodos &&
+                    <FocusZone direction={ FocusZoneDirection.vertical }>
+                        <List items={allTodos} onRenderCell={this.renderCell} />
+                    </FocusZone>
+                }
+            </div>
+        )
+    }
 
-                {allTodos && allTodos.map((todo) => {
-                    return <div key={todo.todoId}>{todo.name}</div>
-                })}
+    renderCell = (item) => {
+        return (
+            <div>
+                <div>{item.name}</div>
+                <div>{item.description}</div>
             </div>
         )
     }
