bc66f691c4e6fff3613b8a1c9d63b174d53b45d5
alexknipfer <alexanderknipfer@gmail.com>
2018-01-09 20:51:37 -0600

    Initialized ts project

---
+++ ts/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        Hello
+      </div>
+    )
+  }
+}
+
+export default App;
