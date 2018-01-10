9e53088f432fa8089d504cadfa4bb7c9bfdf3030
alexknipfer <alexanderknipfer@gmail.com>
2018-01-09 22:20:03 -0600

    Setup AWS client

--- ts/src/index.tsx
+++ ts/src/index.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './App'
+import App from './routes'
 import registerServiceWorker from './registerServiceWorker'
 
+import  AWSAppSyncClient from "aws-appsync";
+import { Rehydrated } from 'aws-appsync-react';
+import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
+import { ApolloProvider } from 'react-apollo';
+import awsConfig from './lib/awsConfig'
+
+const client = new AWSAppSyncClient({
+  url: `https://${awsConfig.HOST}${awsConfig.PATH}`,
+  region: awsConfig.REGION,
+  auth: {type: AUTH_TYPE.API_KEY, apiKey: awsConfig.AWS_API_KEY}
+})
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+        <Rehydrated>
+            <App />
+        </Rehydrated>
+    </ApolloProvider>, 
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
