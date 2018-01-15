3254e6ceecb19b0726919b3be62d00dd8ad746dc
alexknipfer <alexanderknipfer@gmail.com>
2018-01-14 19:30:22 -0600

    Subscribe to deleted todos

--- src/routes/Todos/index.ts
+++ src/routes/Todos/index.ts
@@ -2,6 +2,8 @@ import Todos from './Todos'
 import AllTodosQuery from './queries/allTodosQuery'
 import CreateTodoMutation from './mutations/createTodoMutation'
 import NewTodoSubscription from './subscriptions/newTodoSubscription'
+import DeleteTodoSubscription from './subscriptions/deleteTodoSubscription'
+import DeleteTodoMutation from './mutations/deleteTodoMutation'
 import { compose, graphql } from 'react-apollo'
 import { QueryResponse, WrappedProps, Previous, Todo } from './types'
 
@@ -18,6 +20,16 @@ const withTodos = graphql<QueryResponse, WrappedProps>(AllTodosQuery, {
                 updateQuery: (prev: Previous, { subscriptionData: { data: { newTodo } }}) => ({
                     ...prev,
                     allTodos: [newTodo, ...prev.allTodos.filter(todo => todo.todoId !== newTodo.todoId)]
+                        .sort((a: Todo, b: Todo) => +new Date(b.dateCreated) - +new Date(a.dateCreated))
+                })
+            })
+        },
+        subscribeToDeletedTodo: params => {
+            data.subscribeToMore({
+                document: DeleteTodoSubscription,
+                updateQuery: (prev: Previous, { subscriptionData: { data: { deletedTodo } }}) => ({
+                    ...prev,
+                    allTodos: [...prev.allTodos.filter(todo => todo.todoId !== deletedTodo.todoId)]
                         .sort((a: Todo, b: Todo) => +new Date(b.dateCreated) - +new Date(a.dateCreated))
                 })
             })
@@ -40,4 +52,16 @@ const withCreateTodoMutation = graphql(CreateTodoMutation, {
     })
 })
 
-export default compose(withTodos, withCreateTodoMutation)(Todos)
+const withDeleteTodoMutation = graphql(DeleteTodoMutation, {
+    props: ({ mutate }) => ({
+        deleteTodo: (todoId: string) => {
+            return mutate ? mutate({
+                variables: {
+                    todoId
+                }
+            }) : {}
+        }
+    })
+})
+
+export default compose(withTodos, withCreateTodoMutation, withDeleteTodoMutation)(Todos)
