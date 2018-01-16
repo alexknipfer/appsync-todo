6f55961d42a8b47a59b4d172738803e7e751929c
alexknipfer <alexanderknipfer@gmail.com>
2018-01-15 21:10:34 -0600

    Added amplify config

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,21 @@ import * as ReactDOM from 'react-dom'
 import App from './routes'
 import registerServiceWorker from './registerServiceWorker'
 
+import Amplify from 'aws-amplify'
 import  AWSAppSyncClient from 'aws-appsync'
 import { Rehydrated } from 'aws-appsync-react'
 import { AUTH_TYPE } from 'aws-appsync/lib/link/auth-link'
 import { ApolloProvider } from 'react-apollo'
 import awsConfig from './lib/awsConfig'
+import awsIdentityPoolConfig from './lib/awsIdentityPoolConfig'
 import './lib/ant.css'
+
+Amplify.configure({
+  Auth: {
+    identityPoolId: awsIdentityPoolConfig.AWS_POOL_ID,
+    region: awsIdentityPoolConfig.AWS_POOL_REGION
+  }
+})
 
 const client = new AWSAppSyncClient({
   url: `https://${awsConfig.HOST}${awsConfig.PATH}`,
